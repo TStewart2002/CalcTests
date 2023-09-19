fc38646dcb61660cf3f9eaf8760a80a9d6542a11
Tyler Stewart <z1886436@students.niu.edu>
2023-09-18 23:54:02 -0500

    Working add & mult of ints w/ timer

--- CalcTests.cs
+++ CalcTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 
 class CalcTests
 {
@@ -12,17 +13,43 @@ class CalcTests
 
         numbers = GenRanNumbers(n);     //Fill our array with random numbers
 
-        for (int i = 0; i < n; i++)
-        {
-                Console.WriteLine(numbers[i,0] + "              " + numbers[i,1]);
-        }
 
+        timer.Start();
+        AddNums(numbers, n);        //Perform the adding operation and time it
+        timer.Stop();
         
+        Console.WriteLine("Integer Additions");
+        Console.WriteLine(timer.ElapsedMilliseconds + " ms");       //Display the time it took for integer addition
+        Console.WriteLine(timer.ElapsedTicks + " ticks");
+        float addTicks = timer.ElapsedTicks; 
+
+
+
+
+        timer.Restart(); 
+        timer.Start();
+        MultNums(numbers, n);       //Perform the multiplying operation and time it
+        timer.Stop();
+
+        Console.WriteLine("\n \n");
+        Console.WriteLine("Integer Multiplication");
+        Console.WriteLine(timer.ElapsedMilliseconds + " ms");       //Display the time it took for integer multiplication
+        Console.WriteLine(timer.ElapsedTicks + " ticks");
+        float multTicks = timer.ElapsedTicks;
+
+
+        //Find the ration of adding vs. multiplying and display it
+        float ratio = addTicks / multTicks;
+        
+        Console.WriteLine("\n \n");
+        Console.WriteLine("Ratio (Add vs. Mult):");
+        Console.WriteLine(ratio);
+
 
     }
 
 
-    //Function that generates an array of random numbers
+    //Function that generates an array of random integers
     static int[,] GenRanNumbers(int count)
     {
         Random rand = new Random();      // instantiate random number generator
@@ -39,4 +66,25 @@ class CalcTests
         return num;
     }
 
+
+
+    //Function that adds two integers
+    static void AddNums(int[,] nums, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            nums[i,2] = nums[i,0] + nums[i,1];
+        }
+    }
+
+
+
+    //Function that multiplies two integers
+    static void MultNums(int[,] nums, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            nums[i,2] = nums[i,0]*nums[i,1];
+        }
+    }
 }
